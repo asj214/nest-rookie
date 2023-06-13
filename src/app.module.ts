45dda14dec58b6557845ae6305e3df6cc045c89c
sjahn <asj214@naver.com>
2023-06-13 15:49:30 +0900

    typeorm 패키지 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import typeORMConfig from 'ormconfig';
 
 @Module({
   imports: [
@@ -9,6 +11,7 @@ import { AppService } from './app.service';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot(typeORMConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
