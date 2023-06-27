628ee93dff0219b74e97d00b713c6073d9497b97
sjahn <asj214@naver.com>
2023-06-27 10:46:34 +0900

    product

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { Product } from './entities/product.entity';
+import { Category } from 'src/category/entities/category.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Product, Category])
+  ],
   controllers: [ProductController],
   providers: [ProductService]
 })
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,77 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository, In } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SearchProductDto, CreateProductDto, UpdateProductDto } from './dto/product.dto';
+import { Product } from './entities/product.entity';
+import { Category } from 'src/category/entities/category.entity';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>
+  ) {}
+
+  async create(user, dto: CreateProductDto) {
+    const product = new Product();
+    product.user = user;
+    product.name = dto.name;
+    product.price = dto.price;
+
+    const categories = await this.categoryRepository.find({
+      where: {
+        id: In(dto.categoryIds)
+      }
+    });
+    product.categories = categories;
+
+    return await this.productRepository.save(product);
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll(query: SearchProductDto) {
+    const { name, categoryId, take, skip } = query;
+
+    const qb = this.productRepository.createQueryBuilder('product');
+    qb.leftJoinAndSelect('product.categories', 'category')
+
+    qb.where('1=1')
+    if (name) qb.andWhere('product.name LIKE :name', { name: `%${name}%` })
+    if (categoryId) qb.andWhere('category.id = :categoryId', { categoryId })
+
+    qb.orderBy('product.id', 'DESC');
+
+    const count = await qb.getCount();
+
+    qb.take(take);
+    qb.skip(skip)
+
+    const data = await qb.getMany();
+    return {
+      count: count,
+      data: data
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      relations: ['categories'],
+      where: {
+        id: id
+      }
+    });
+
+    if (!product) throw new NotFoundException();
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, dto: UpdateProductDto) {
+    return await this.productRepository.update(id, dto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    return await this.productRepository.softDelete({ id: id });
   }
 }
